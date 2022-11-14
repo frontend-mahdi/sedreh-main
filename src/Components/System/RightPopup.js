26d0b0bbcbb4921df07a1df3af87cecb99678679
frontend-mahdi <mahdi.313.ab@gmail.com>
2022-11-14 17:48:28 +0330

    save pic form css fixed

--- src/Components/System/RightPopup.js
+++ src/Components/System/RightPopup.js
@@ -38,37 +38,41 @@ export default function RightPopup(props) {
       >
         {(close) => (
           <div className="modal">
-            <button className="close text-white" onClick={close}>
+            <button className="close text-white mt-2 ml-4" onClick={close}>
               &times;
             </button>
             <form>
-              <div
-                dir="rtl"
-                className="flex flex-col justify-center items-center"
-              >
-                <p className="text-gray mb-2">
-                  نام تصویر مورد نظر را بنویسید :
-                </p>
+              <div dir="rtl" className="px-8">
+                <div>
+                  <label className="text-gray text-right">
+                    نام تصویر مورد نظر را بنویسید :
+                  </label>
 
-                <input
-                  type="text"
-                  className="w-3/4 h-8 bg-gray-background mb-2 text-gray"
-                  placeholder="نام تصویر ..."
-                  onChange={(event) => setFirstName(event.target.value)}
-                  value={firstName}
-                  required
-                />
-                <p className="text-gray mb-2">توضیحات مورد نظر را بنویسید :</p>
-                <input
-                  type="text"
-                  className="w-3/4 h-28 bg-gray-background mb-2 text-gray flex justify-around items-baseline"
-                  placeholder="توضیحات"
-                  value={lastName}
-                  onChange={(event) => setLastName(event.target.value)}
-                />
+                  <input
+                    type="text"
+                    className="px-2 py-1 w-full h-8 mt-2 bg-gray-background mb-4 text-gray"
+                    placeholder="نام تصویر ..."
+                    onChange={(event) => setFirstName(event.target.value)}
+                    value={firstName}
+                    required
+                  />
+                </div>
+                <div>
+                  <label className="text-gray text-right">
+                    توضیحات مورد نظر را بنویسید :
+                  </label>
+                  <input
+                    type="text"
+                    className="px-2 py-1 w-full h-28 mt-2 bg-gray-background mb-4 text-gray"
+                    placeholder="توضیحات"
+                    value={lastName}
+                    onChange={(event) => setLastName(event.target.value)}
+                  />
+                </div>
                 <button
+                  disabled={firstName.length == 0}
                   type="button"
-                  className="bg-primarythree w-32 h-10 rounded-lg flex justify-center items-center mt-2 mb-2 text-black"
+                  className="bg-primarythree px-10 disabled:text-grayer mx-auto  disabled:bg-gray h-10 rounded-lg flex justify-center items-center mt-2 mb-4 text-black"
                   onClick={(event) => {
                     close();
                     event.preventDefault();
